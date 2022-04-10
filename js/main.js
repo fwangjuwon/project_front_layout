0ea618e1af55be882e5112dc711449e6b6bf11c8
tjdgus <tjdgus7562@naver.com>
2022-04-10 23:39:26 +0900

    자러가장

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
         //지도 옵션
         var mapOptions = {
             center: new naver.maps.LatLng(35.159665, 129.060447),
-            zoom: 10,
+            zoom: 15,
             // 커스텀 컨트롤러 생성
 
 
@@ -15,10 +15,76 @@
             }
         };
         // 커스텀 컨트롤러 생성
-        var locationBtnHtml = `<div class="m_box_search_map">인포박스</div>`;
+        var locationBtnHtml = ` <div class="m_box_search_map">
+        <div class="text-center m_bg_secondary m_box_lg_up_round" style="border-top-right-radius: 25px;">
+            <div class="m_ph_sm">
+                <h3>검색</h3>
+            </div>
+            <form class="container">
+                <input class="form-control m_ph_sm" type="text" placeholder="주소 또는 명칭으로 검색하실 수 있습니다">
+            </form>
+            <div class="d-flex m_box_search_category">
+                <a href="#">
+                    <div>
+                        <i class="fa-solid fa-flag"></i>
+                    </div>
+                    <div>관광</div>
+                </a>
+                <a href="#">
+                    <div>
+                        <i class="fa-solid fa-bed"></i>
+                    </div>
+                    <div>숙박</div>
+                </a>
+                <a href="#">
+                    <div>
+                        <i class="fa-solid fa-mug-hot"></i>
+                    </div>
+                    <div>식음료</div>
+                </a>
+                <a href="#">
+                    <div>
+                        <i class="fa-solid fa-volleyball"></i>
+                    </div>
+                    <div>체험</div>
+                </a>
+                <a href="#">
+                    <div>
+                        <i class="fa-solid fa-house-chimney-medical"></i>
+                    </div>
+                    <div>동물병원</div>
+                </a>
+            </div>
+        </div>
+        <div class = "m_box_check_category">
+            <div class="m_pl_sm hr-sect">주변 탐색</div>
+            <div class="m_pl_md" >
+                <div class="form-check m_pb_sm">
+                    <input class="form-check-input" type="checkbox" id="check1" name="option1" value="something" checked>
+                    <label class="form-check-label">관광지</label>
+                </div>
+                <div class="form-check m_pb_sm">
+                    <input class="form-check-input" type="checkbox" id="check1" name="option2" value="something" checked>
+                    <label class="form-check-label">숙박</label>
+                </div>
+                <div class="form-check m_pb_sm">
+                    <input class="form-check-input" type="checkbox" id="check1" name="option3" value="something" checked>
+                    <label class="form-check-label">식음료</label>
+                </div>
+                <div class="form-check m_pb_sm">
+                    <input class="form-check-input" type="checkbox" id="check1" name="option4" value="something" checked>
+                    <label class="form-check-label">체험</label>
+                </div>
+                <div class="form-check m_pb_sm">
+                    <input class="form-check-input" type="checkbox" id="check1" name="option5" value="something" checked>
+                    <label class="form-check-label">동물병원</label>
+                </div>
+            </div>
+        </div>
+    </div>`;
 
         var customControl = new naver.maps.CustomControl(locationBtnHtml, {
-            position: naver.maps.Position.LEFT_CENTER
+            position: naver.maps.Position.TOP_LEFT
         });
 
         // 지도 생성
